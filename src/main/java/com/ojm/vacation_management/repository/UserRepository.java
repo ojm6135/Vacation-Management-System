e720d2abda1ddf9897bcbfdfc866a7ff5be9ab88
ojm6135 <ojm6135@naver.com>
2024-12-15 19:43:09 +0900

    UserRepository, UserRepositoryImpl에 role, status 수정 메서드 추가, UserRepositoryImpl의 findAll, findAllByStatus jpql을 querydsl로 변경.

--- src/main/java/com/ojm/vacation_management/repository/UserRepository.java
+++ src/main/java/com/ojm/vacation_management/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.ojm.vacation_management.repository;
 
 import com.ojm.vacation_management.domain.User;
+import com.ojm.vacation_management.vo.user.UserRole;
 import com.ojm.vacation_management.vo.user.UserStatus;
 
 import java.util.List;
@@ -11,4 +12,7 @@ public interface UserRepository {
     Optional<User> findById(int id);
     List<User> findAll();
     List<User> findAllByStatus(UserStatus status);
+
+    void updateUserRole(int id, UserRole updatedRole);
+    void updateUserStatus(int id, UserStatus updatedStatus);
 }
--- src/main/java/com/ojm/vacation_management/repository/UserRepositoryImpl.java
+++ src/main/java/com/ojm/vacation_management/repository/UserRepositoryImpl.java
@@ -1,21 +1,30 @@
 package com.ojm.vacation_management.repository;
 
+import com.ojm.vacation_management.domain.QUser;
 import com.ojm.vacation_management.domain.User;
+import com.ojm.vacation_management.vo.user.UserRole;
 import com.ojm.vacation_management.vo.user.UserStatus;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
     private final EntityManager em;
 
+    private final JPAQueryFactory queryFactory;
+    private final QUser user;
+
     @Autowired
-    public UserRepositoryImpl(EntityManager em) {
+    public UserRepositoryImpl(EntityManager em, JPAQueryFactory queryFactory) {
         this.em = em;
+        this.queryFactory = queryFactory;
+        this.user = QUser.user;
     }
 
     @Override
@@ -30,13 +39,34 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public List<User> findAll() {
-        return em.createQuery("select u from User u", User.class).getResultList();
+        return queryFactory.selectFrom(user).fetch();
     }
 
     @Override
     public List<User> findAllByStatus(UserStatus status) {
-        return em.createQuery("select u from User u where u.status = :status", User.class)
-                .setParameter("status", status)
-                .getResultList();
+        return queryFactory.selectFrom(user)
+                .where(user.status.eq(status))
+                .fetch();
+    }
+
+    @Override
+    public void updateUserRole(int id, UserRole updatedRole) {
+        Optional<User> userOptional = findById(id);
+        userOptional.ifPresentOrElse(user -> {
+            user.setRole(updatedRole);
+        }, () -> {
+            throw new NoSuchElementException("id가 " + id + "인 회원이 존재하지 않습니다.");
+        });
+    }
+
+    @Override
+    public void updateUserStatus(int id, UserStatus updatedStatus) {
+        Optional<User> userOptional = findById(id);
+        userOptional.ifPresentOrElse(user -> {
+            user.setStatus(updatedStatus);
+        }, () -> {
+            throw new NoSuchElementException("id가 " + id + "인 회원이 존재하지 않습니다.");
+        });
     }
 }
+
