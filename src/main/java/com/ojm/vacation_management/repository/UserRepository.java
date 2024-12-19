2039fd3b578d49fcd6b478d5b16b526d7dd1a5be
ojm6135 <ojm6135@naver.com>
2024-12-19 16:34:13 +0900

    UserRepositoryImpl findByUsername 추가

--- src/main/java/com/ojm/vacation_management/repository/UserRepository.java
+++ src/main/java/com/ojm/vacation_management/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.ojm.vacation_management.repository;
 
 import com.ojm.vacation_management.domain.User;
+import com.ojm.vacation_management.dto.UserDto;
+import com.ojm.vacation_management.dto.UserRegistrationDto;
+import com.ojm.vacation_management.form.UserRegistrationForm;
 import com.ojm.vacation_management.vo.user.UserRole;
 import com.ojm.vacation_management.vo.user.UserStatus;
 
@@ -10,9 +13,9 @@ import java.util.Optional;
 public interface UserRepository {
     void save(User user);
     Optional<User> findById(int id);
+    Optional<User> findByUsername(String username);
     List<User> findAll();
     List<User> findAllByStatus(UserStatus status);
-
     void updateUserRole(int id, UserRole updatedRole);
     void updateUserStatus(int id, UserStatus updatedStatus);
 }
