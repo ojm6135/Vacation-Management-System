43aa0f7e86821811df7527db65c2acb2edeb2f33
ojm6135 <ojm6135@naver.com>
2024-12-21 20:56:56 +0900

    UserRegistrationForm validation 메시지 추가

--- src/main/java/com/ojm/vacation_management/form/UserRegistrationForm.java
+++ src/main/java/com/ojm/vacation_management/form/UserRegistrationForm.java
@@ -1,6 +1,7 @@
 package com.ojm.vacation_management.form;
 
 import com.ojm.vacation_management.vo.user.UserRole;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -10,27 +11,20 @@ import org.hibernate.validator.constraints.Length;
 @Getter
 @AllArgsConstructor
 public class UserRegistrationForm {
-    @Length(min = 8, max = 15)
-    @Pattern(regexp = "[\\w_.-]+")
+    @NotBlank(message = "아이디가 필요합니다.")
+    @Length(min = 8, max = 15, message = "형식이 맞지 않습니다.")
+    @Pattern(regexp = "[\\w_.-]+", message = "형식이 맞지 않습니다.")
     private final String username;
 
-    @Length(min = 10, max = 15)
-    @Pattern(regexp = "[\\w!@#$%^&*_=+.,?-]+")
+    @NotBlank(message = "비밀번호가 필요합니다.")
+    @Length(min = 10, max = 15, message = "형식이 맞지 않습니다.")
+    @Pattern(regexp = "[\\w!@#$%^&*_=+.,?-]+", message = "형식이 맞지 않습니다.")
     private final String password;
 
-    @Pattern(regexp = "\\S[a-zA-z가-힣 ]+\\S$")
+    @NotBlank(message = "이름이 필요합니다.")
+    @Pattern(regexp = "\\S[a-zA-z가-힣 ]+\\S$", message = "형식이 맞지 않습니다.")
     private final String name;
 
-    @NotNull
+    @NotNull(message = "회원 유형이 필요합니다.")
     private final UserRole role;
-
-    @Override
-    public String toString() {
-        return "UserRegistrationForm{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                ", role=" + role +
-                '}';
-    }
 }
