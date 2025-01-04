11392408e55f7c4fa1d95381ea8ff245d4395e8d
ojm6135 <ojm6135@naver.com>
2025-01-04 20:26:18 +0900

    VacationForm 추가, VacationDto에 entity, form 객체 간 변환 메소드 추가.

--- src/main/java/com/ojm/vacation_management/dto/VacationDto.java
+++ src/main/java/com/ojm/vacation_management/dto/VacationDto.java
@@ -1,14 +1,17 @@
 package com.ojm.vacation_management.dto;
 
 import com.ojm.vacation_management.domain.Vacation;
+import com.ojm.vacation_management.form.VacationForm;
 import com.ojm.vacation_management.vo.vacation.AppliedVacationStatus;
 import com.ojm.vacation_management.vo.vacation.VacationPeriod;
 import com.ojm.vacation_management.vo.vacation.VacationType;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class VacationDto {
     private int id;
     private int userId;
@@ -17,12 +20,36 @@ public class VacationDto {
     private String reason;
     private AppliedVacationStatus status;
 
-    public VacationDto(Vacation vacation) {
-        this.id = vacation.getId();
-        this.userId = vacation.getUserId();
-        this.period = vacation.getPeriod();
-        this.type = vacation.getType();
-        this.reason = vacation.getReason();
-        this.status = vacation.getStatus();
+    public static Vacation toEntity(VacationDto dto) {
+        return Vacation.builder()
+                .id(dto.getId())
+                .userId(dto.getUserId())
+                .period(dto.getPeriod())
+                .type(dto.getType())
+                .reason(dto.getReason())
+                .status(dto.getStatus())
+                .build();
+    }
+
+    public static VacationDto fromEntity(Vacation entity) {
+        return VacationDto.builder()
+                .id(entity.getId())
+                .userId(entity.getUserId())
+                .period(entity.getPeriod())
+                .type(entity.getType())
+                .reason(entity.getReason())
+                .status(entity.getStatus())
+                .build();
+    }
+
+    public static VacationDto fromForm(VacationForm form) {
+        VacationPeriod period = new VacationPeriod(form.getStartDate(), form.getEndDate());
+
+        return VacationDto.builder()
+                .userId(form.getUserId())
+                .period(period)
+                .type(form.getType())
+                .reason(form.getReason())
+                .build();
     }
 }
---
+++ src/main/java/com/ojm/vacation_management/form/VacationForm.java
@@ -0,0 +1,31 @@
+package com.ojm.vacation_management.form;
+
+import com.ojm.vacation_management.vo.vacation.VacationType;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+public class VacationForm {
+    @NotNull(message = "user id가 필요합니다.")
+    private final int userId;
+
+    @NotNull(message = "시작 날짜가 입력되지 않았습니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private final LocalDate startDate;
+
+    @NotNull(message = "종료 날짜가 입력되지 않았습니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private final LocalDate endDate;
+
+    @NotNull(message = "휴가 종류가 입력되지 않았습니다.")
+    private final VacationType type;
+
+    @Nullable
+    private final String reason;
+}
